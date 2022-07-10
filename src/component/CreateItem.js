3dc1f17c34d01e0294dcc71606676fce8717bae0
Murtaza Pakawala <mypaka52@gmail.com>
2022-07-10 11:08:18 +1000

    create item route design and working

--- src/component/CreateItem.js
+++ src/component/CreateItem.js
@@ -1,7 +1,54 @@
 import React from "react";
-
+import { useState } from "react";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 function CreateItem() {
-  return <div>CreateItem.</div>;
+  const [title, setTitle] = useState();
+  const [price, setPrice] = useState();
+  const [img, setImg] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(`title ${title}`);
+    console.log(`price ${price}`);
+  }
+  return (
+    <div className="create-post">
+      <h1>New Item Details</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Item Name</label>
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+        <div className="price-field">
+          <label>price</label>
+          <br />
+          <AttachMoneyIcon />
+          <input
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-field">
+          <label>Image Link</label>
+          <input
+            value={img}
+            onChange={(e) => {
+              setImg(e.target.value);
+            }}
+          />
+        </div>
+        <button className="create-post-btn">Create Item</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateItem;
