1684415b81a0d659d3f0f482a8d0bf1c650c5f09
Murtaza Pakawala <mypaka52@gmail.com>
2022-07-10 16:10:23 +1000

    home: styled the home page and showing the date from the firebase datastore

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,43 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import { firestore } from "../firebase";
+import { Link } from "react-router-dom";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 function Home() {
-  return <div>Home</div>;
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    firestore
+      .collection("items")
+      .get()
+      .then((snapshot) => {
+        const items = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+
+        console.log("items", items);
+        setItems(items);
+      });
+  }, []);
+  return (
+    <div className="home">
+      <h1>Items Display</h1>
+
+      {items.map((item, index) => (
+        <div className="item" key={`item-${index}`}>
+          <img width={200} height={150} src={item.img} alt="cant load"></img>
+          <Link to={`/item/${item.id}`}>
+            <h3>{item.title}</h3>
+          </Link>
+          <AttachMoneyIcon />
+          <p>{item.price}</p>
+          <button>Sell</button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
